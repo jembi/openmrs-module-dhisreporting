0d5617cfaab3e72ab9d3fc4086a71e0e461c89b3
k-joseph <kaweesi@jembi.org>
2016-10-31 10:58:25 +0300

    Added lab form gps and setting up things

---
+++ api/src/main/java/org/openmrs/module/dhisreporting/DHISReportingGPConstants.java
@@ -0,0 +1,35 @@
+package org.openmrs.module.dhisreporting;
+
+public class DHISReportingGPConstants {
+	private static String BLOODSMEAR_CONCEPTID = "dhisreporting.bloodSmearConceptId";
+
+	private static String MICROFILARIA_CONCEPTID = "dhisreporting.microFilariaConceptId";
+
+	private static String TRYPANOSOMA_CONCEPTID = "dhisreporting.trypanosomaConceptId";
+
+	private static String GIARDIA_CONCEPTID = "dhisreporting.giardiaConceptId";
+
+	private static String ASCARIASIS_CONCEPTID = "dhisreporting.ascariasisConceptId";
+
+	private static String ANKLYOSTIASIS_CONCEPTID = "dhisreporting.anklyostiasisConceptId";
+
+	private static String TAENIA_CONCEPTID = "dhisreporting.taeniaConceptId";
+
+	private static String OTHERPARASITES_CONCEPTID = "dhisreporting.otherParasitesConceptId";
+
+	private static String PREGNANCYTEST_CONCEPTID = "dhisreporting.pregnancyTestConceptId";
+
+	private static String HEMOGLOBIN_CONCEPTID = "dhisreporting.hemoglobinConceptId";
+
+	private static String FULLBLOODCOUNT_CONCEPTID = "dhisreporting.fullBloodCountConceptId";
+
+	private static String CREATINE_CONCEPTID = "dhisreporting.creatinineConceptId";
+
+	private static String AMYLASSE_CONCEPTID = "dhisreporting.amylasseConceptId";
+
+	private static String CD4COUNT_CONCEPTID = "dhisreporting.cd4CountConceptId";
+
+	private static String WIDAL_CONCEPTID = "dhisreporting.widalConceptId";
+
+	private static String DEREBROSPINALFLUID_CONCEPTID = "dhisreporting.derebrospinalFluidConceptId";
+}
--- api/src/main/java/org/openmrs/module/dhisreporting/api/impl/DHISReportingServiceImpl.java
+++ api/src/main/java/org/openmrs/module/dhisreporting/api/impl/DHISReportingServiceImpl.java
@@ -13,32 +13,54 @@
  */
 package org.openmrs.module.dhisreporting.api.impl;
 
-import org.openmrs.api.impl.BaseOpenmrsService;
+import java.util.Collections;
+import java.util.Date;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.Cohort;
+import org.openmrs.Concept;
+import org.openmrs.Location;
+import org.openmrs.api.PatientSetService.TimeModifier;
+import org.openmrs.api.context.Context;
+import org.openmrs.api.impl.BaseOpenmrsService;
 import org.openmrs.module.dhisreporting.api.DHISReportingService;
 import org.openmrs.module.dhisreporting.api.db.DHISReportingDAO;
+import org.openmrs.module.reporting.cohort.definition.CodedObsCohortDefinition;
+import org.openmrs.module.reporting.cohort.definition.service.CohortDefinitionService;
 
 /**
  * It is a default implementation of {@link DHISReportingService}.
  */
 public class DHISReportingServiceImpl extends BaseOpenmrsService implements DHISReportingService {
-	
+
 	protected final Log log = LogFactory.getLog(this.getClass());
-	
+
 	private DHISReportingDAO dao;
-	
+
 	/**
-     * @param dao the dao to set
-     */
-    public void setDao(DHISReportingDAO dao) {
-	    this.dao = dao;
-    }
-    
-    /**
-     * @return the dao
-     */
-    public DHISReportingDAO getDao() {
-	    return dao;
-    }
+	 * @param dao
+	 *            the dao to set
+	 */
+	public void setDao(DHISReportingDAO dao) {
+		this.dao = dao;
+	}
+
+	/**
+	 * @return the dao
+	 */
+	public DHISReportingDAO getDao() {
+		return dao;
+	}
+
+	public void saveObsCountCohortQuery(Concept concept, Date startDate, Date endDate, Location location) {
+		CodedObsCohortDefinition cd = new CodedObsCohortDefinition();
+
+		cd.setTimeModifier(TimeModifier.AVG);
+		cd.setQuestion(concept);
+		cd.setOnOrAfter(startDate);
+		cd.setOnOrBefore(endDate);
+		cd.setLocationList(Collections.singletonList(location));
+		//Cohort cohort = Context.getService(CohortDefinitionService.class).saveDefinition(cd);
+	}
 }
